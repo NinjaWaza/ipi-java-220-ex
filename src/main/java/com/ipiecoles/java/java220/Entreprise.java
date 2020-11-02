d295de955066a450e7356afcea02a158cc24070c
Théodore <theodore.follet@gmail.com>
2020-11-02 11:19:44 +0100

    Exercice 0

---
+++ src/main/java/com/ipiecoles/java/java220/Entreprise.java
@@ -0,0 +1,19 @@
+package com.ipiecoles.java.java220;
+
+import java.time.LocalDate;
+
+public class Entreprise {
+
+    //le final permet de dire que la variable est une constante
+
+    public static final Double SALAIRE_BASE = 1480.27;
+    public static final Integer NB_CONGES_BASE = 25;
+    public static final Double INDICE_MANAGER = 1.3;
+    public static final Double PRIME_MANAGER_PAR_TECHNICIEN = 250.0;
+    public static final Double PRIME_ANCIENNETE = 100d;
+
+//    public static double primeAnnuelleBase(){
+//        int annee =  LocalDate.now().getYear();
+//        return annee/2;
+//    }
+}
---
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -0,0 +1,28 @@
+package com.ipiecoles.java.java220;
+
+public class Main{
+    public static void main(String[] args){
+//        String chaineNonInitialisee = null;
+//
+//
+//        String chaine = "Hello world !";
+//        System.out.println(chaine.hashCode());
+//        chaine = null;
+//        chaine = "Hello world !";
+//        System.out.println(chaine.hashCode());
+//        chaine = null;
+//        chaine = new String("Hello world !");
+//        System.out.println(chaine.hashCode());
+//        if (chaine == "Hello world !"){
+//            //Compare les références
+//            System.out.println(chaine);
+//        }
+//        if (chaine.equals("Hello world !")){
+//            //Compare les valeurs
+            //Donc le new String vas pas posé problème car en finalité c'est Hello World !
+//            System.out.println(chaine);
+//        }
+        //System.out.println("Hello world !");
+        System.out.println(Entreprise.NB_CONGES_BASE);
+    }
+}
